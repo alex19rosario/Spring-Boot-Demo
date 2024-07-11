49973b30b2b8df04e7b32299e1f6d02ce0b5791c
Carlos Rosario <alexrosario7130492@gmail.com>
2024-07-11 07:38:08 -0400

    Restricting endpoints based on roles using the SecurityFilterChain

--- src/main/java/com/example/demo_backend/security/DemoSecurityConfig.java
+++ src/main/java/com/example/demo_backend/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.example.demo_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -28,4 +32,23 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/books").hasRole("ANALYST")
+                .requestMatchers(HttpMethod.GET, "/api/books/**").hasRole("ANALYST")
+                .requestMatchers(HttpMethod.POST, "/api/books").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN"));
+
+        // use Http basic authorization
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // In general, not required for stateless REST APIs that use GET, POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
 }
